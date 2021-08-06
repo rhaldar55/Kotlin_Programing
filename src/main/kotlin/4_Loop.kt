c9a826c72db3613fea6b023434a6697580261717
rhaldar <rhaldar@gmail.com>
2021-08-06 18:31:41 +0530

    Loops

---
+++ src/main/kotlin/4_Loop.kt
@@ -0,0 +1,23 @@
+fun main() {
+
+    //FOR LOOP
+    for(i in 1..4){
+        println("Hello")
+    }
+
+    //WHILE LOOP
+    var i: Int = 1
+    while (i<10){
+        println(i)
+        i++
+    }
+
+    //DO WHILE LOOP
+    var j: Int = 1
+    do{
+        println(j)
+        j++
+    }while (j<5)
+
+
+}
